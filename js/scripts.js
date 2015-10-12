3316a5c0e92556b58a820ad4ad243a01f2f92156
Mim Harwood <mim.harwood@gmail.com>
2015-10-11 21:15:54 -0400

    javascript practice

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+//example of variable below
+//variables are containers, like x
+
+x = 5
+x= 5.50
+x=5.555
+
+/*Datatypes:
+Numbers (integers, doubles, floats)
+Strings (text in quotes)
+Booleans [t/f statements]
+Array [list of variables]
+Objects
+*/
+
+//Booleans are statements that are true or false
+
+islighton = true
+
+//arrays are lists of elements
+
+var arr =["M", "T", "W", "Th", "F"]
+
+var arr = [
+   arr[0] = "M",
+   arr[1] = "T",
+   arr[2] = "w",
+   arr[3] = "Th",
+   arr[4] - "F"
+]
+
+//below should output "Th"
+console.log(arr[3])
+  
+//below are ways we can test in the console
+
+if(5>10) {
+	console.log 
+	//this won't show up because the statement is false
+}
+
+if (5<10) {
+	console.log
+	//this will show up in the console because it is true
+}
+
+
+
+/*below is an example of a function.
+A function is a way to encapsulate code for later use.
+*/
+
+numberOne = 4;
+otherNumber = 14;
+
+function someName(numberOne, otherNumber){
+	return numberOne + 10 + otherNumber;
+}
+
+console.log(someName(4, 14));
+
+//below are some practice examples using alert and console
+
+var x = 5;
+x = 10;
+x= x + 10;
+console.log( x );
+
+var firstName = "Mim";
+console.log( firstName );
+
+var isLightOn = true;
+console.log( isLightOn );
+
+alert("Hello World");
+console.log("Hi!!!");
+
+
+
